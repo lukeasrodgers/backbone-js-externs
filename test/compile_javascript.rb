74bdbeeaf148ef52325d68f565201a837f089691
Luke Rodgers <lukeasrodgers@gmail.com>
2012-12-16 14:32:50 -0500

    Adding tests, externs

---
+++ test/compile_javascript.rb
@@ -0,0 +1,33 @@
+require 'fileutils'
+
+base = "#{File.expand_path(File.dirname(__FILE__))}/../"
+externs_dir = base
+
+# compile base command
+cc_path = ENV['CC_PATH']
+javascript_compiler_cmd = "java -jar #{cc_path}"
+
+# do trial compilation with stringent closure compiler options
+cmd ="cd #{base} &&"
+cmd << javascript_compiler_cmd
+cmd << " --compilation_level=ADVANCED_OPTIMIZATIONS "
+cmd << " --jscomp_off=globalThis "
+cmd << " --jscomp_error=checkTypes "
+cmd << " --externs test/qunit-externs.js"
+cmd << " --externs test/json.js"
+cmd << " --externs test/underscore-1.3.3.js "
+cmd << " --externs test/jquery-1.7-externs.js "
+cmd << " --externs backbone-0.9.2-externs.js "
+cmd << " --js test/collection.js"
+cmd << " --js test/events.js"
+cmd << " --js test/model.js"
+cmd << " --js test/router.js"
+cmd << " --js test/sync.js"
+cmd << " --js test/view.js"
+`#{cmd}`
+compile_okay = $?.exitstatus == 0
+if (!compile_okay)
+  puts "** advanced compilation failed **"
+end
+
+exit(compile_okay)
---
+++ test/qunit-externs.js
@@ -0,0 +1,82 @@
+/**
+ * @param {string} name
+ * @param {Object=} lifecycle
+ */
+function module(name, lifecycle) {}
+
+/**
+ * @param {string} title
+ * @param {Number|Function} expected
+ * @param {Function=} test_func
+ */
+function test(title, expected, test_func){}
+
+/**
+ * @param {string} name
+ * @param {Number|Function} expected
+ * @param {Function=} test_func
+ */
+function asyncTest(name, expected, test_func){}
+
+/**
+ * @param {number} amount
+ */
+function expect(amount){}
+
+/**
+ * @param {*} state
+ * @param {string=} message
+ */
+function ok(state, message){}
+
+/**
+ * @param {*} actual
+ * @param {*} expected
+ * @param {string=} message
+ */
+function equal(actual, expected, message){}
+
+/**
+ * @param {*} actual
+ * @param {*} expected
+ * @param {string=} message
+ */
+function notEqual(actual, expected, message){}
+
+/**
+ * @param {*} actual
+ * @param {*} expected
+ * @param {string=} message
+ */
+function deepEqual(actual, expected, message){}
+
+/**
+ * @param {*} actual
+ * @param {*} expected
+ * @param {string=} message
+ */
+function notDeepEqual(actual, expected, message){}
+
+/**
+ * @param {*} actual
+ * @param {*} expected
+ * @param {string=} message
+ */
+function strictEqual(actual, expected, message){}
+
+/**
+ * @param {*} actual
+ * @param {*} expected
+ * @param {string=} message
+ */
+function notStrictEqual(actual, expected, message){}
+
+/**
+ * @param {number=} increment
+ */
+function start(increment){}
+
+/**
+ * @param {number=} increment
+ */
+function stop(increment){}
